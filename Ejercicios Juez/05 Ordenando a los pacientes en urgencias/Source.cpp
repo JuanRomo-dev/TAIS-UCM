75a3a312281f43093362865f6cabca566233cfa0
LordSama <72462333+LordSama@users.noreply.github.com>
2022-09-20 19:43:46 +0200

    Rename Ejercicios Juez/Source.cpp to Ejercicios Juez/05 Ordenando a los pacientes en urgencias/Source.cpp

--- Ejercicios Juez/05 Ordenando a los pacientes en urgencias/Source.cpp
+++ Ejercicios Juez/05 Ordenando a los pacientes en urgencias/Source.cpp
@@ -12,20 +12,20 @@ using namespace std;
 
 /*@ <answer>
 
- Escribe aquí un comentario general sobre la solución, explicando cómo
- se resuelve el problema y cuál es el coste de la solución, en función
- del tamaño del problema.
+ Escribe aquÃ­ un comentario general sobre la soluciÃ³n, explicando cÃ³mo
+ se resuelve el problema y cuÃ¡l es el coste de la soluciÃ³n, en funciÃ³n
+ del tamaÃ±o del problema.
 
- Tenemos una cola de prioridad, de tipo tPaciente en el que se guarda el nombre del paciente, la gravedad indicando su estado de salud y la espera que le faltaría
- para ser atendido en ese momento, donde los elementos están ordenados de mayor a menor según la gravedad. Vamos a ir leyendo cada evento para saber que hacer en
- cada momento. Si el evento es I, entonces un paciente pasa a la cola, mientras que si el evento es A, el médico atiende al siguiente en la cola.
+ Tenemos una cola de prioridad, de tipo tPaciente en el que se guarda el nombre del paciente, la gravedad indicando su estado de salud y la espera que le faltarÃ­a
+ para ser atendido en ese momento, donde los elementos estÃ¡n ordenados de mayor a menor segÃºn la gravedad. Vamos a ir leyendo cada evento para saber que hacer en
+ cada momento. Si el evento es I, entonces un paciente pasa a la cola, mientras que si el evento es A, el mÃ©dico atiende al siguiente en la cola.
 
- El coste del algoritmo en el caso peor es O(N*M log(N)), siendo N el número de eventos y M el número de pacientes atendidos.
+ El coste del algoritmo en el caso peor es O(N*M log(N)), siendo N el nÃºmero de eventos y M el nÃºmero de pacientes atendidos.
  @ </answer> */
 
 
  // ================================================================
- // Escribe el código completo de tu solución aquí debajo
+ // Escribe el cÃ³digo completo de tu soluciÃ³n aquÃ­ debajo
  // ================================================================
  //@ <answer>
 
@@ -41,7 +41,7 @@ bool operator<(tPaciente const& a, tPaciente const& b) {
 
 bool resuelveCaso() {
     // leer los datos de la entrada
-    int n;              // Número de eventos.
+    int n;              // NÃºmero de eventos.
 
     cin >> n;
     if (n == 0)
@@ -66,13 +66,13 @@ bool resuelveCaso() {
     }
     // resolver el caso posiblemente llamando a otras funciones
     cout << "---\n";
-    // escribir la solución
+    // escribir la soluciÃ³n
 
     return true;
 }
 
 //@ </answer>
-//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+//  Lo que se escriba dejado de esta lÃ­nea ya no forma parte de la soluciÃ³n.
 
 int main() {
     // ajustes para que cin extraiga directamente de un fichero
