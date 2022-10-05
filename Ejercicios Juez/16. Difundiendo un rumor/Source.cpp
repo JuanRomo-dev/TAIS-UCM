07cdf61f871acb7cc6ac1e8cfbd14b082fb0bdf4
LordSama <72462333+LordSama@users.noreply.github.com>
2022-10-05 10:54:54 +0200

    Rename Ejercicios Juez/Source.cpp to Ejercicios Juez/16. Difundiendo un rumor/Source.cpp

--- Ejercicios Juez/16. Difundiendo un rumor/Source.cpp
+++ Ejercicios Juez/16. Difundiendo un rumor/Source.cpp
@@ -1,12 +1,12 @@
 /*
- * MUY IMPORTANTE: Solo se corregir·n los comentarios y el cÛdigo
+ * MUY IMPORTANTE: Solo se corregir√°n los comentarios y el c√≥digo
  * contenidos entre las etiquetas <answer> y </answer>.
- * Toda modificaciÛn fuera de esas etiquetas no ser· corregida.
+ * Toda modificaci√≥n fuera de esas etiquetas no ser√° corregida.
  */
 
  /*@ <answer>
   *
-  * Indicad el nombre completo y usuario del juez de quienes habÈis hecho esta soluciÛn:
+  * Indicad el nombre completo y usuario del juez de quienes hab√©is hecho esta soluci√≥n:
   * Estudiante 1: Juan Romo Iribarren TAIS89
   * Estudiante 2:
   *
@@ -25,32 +25,32 @@ using namespace std;
 
 /*@ <answer>
 
- Escribe aquÌ un comentario general sobre la soluciÛn, explicando cÛmo
- se resuelve el problema y cu·l es el coste de la soluciÛn, en funciÛn
- del tamaÒo del problema.
+ Escribe aqu√≠ un comentario general sobre la soluci√≥n, explicando c√≥mo
+ se resuelve el problema y cu√°l es el coste de la soluci√≥n, en funci√≥n
+ del tama√±o del problema.
 
- El coste de la soluciÛn es O(N + M), siendo N el n˙mero de personajes en Overcity que coincide con el n˙mero de vÈrtices del grafo
- y M el n˙mero de pares de amigos que coincide con el n˙mero de aristas.
+ El coste de la soluci√≥n es O(N + M), siendo N el n√∫mero de personajes en Overcity que coincide con el n√∫mero de v√©rtices del grafo
+ y M el n√∫mero de pares de amigos que coincide con el n√∫mero de aristas.
 
- Los personajes est·n guardados en un grafo, donde cada vÈrtice representa a cada uno de los personajes y cada arista representa
- la conexiÛn entre 2 amigos, es decir, cada par de amigos.
+ Los personajes est√°n guardados en un grafo, donde cada v√©rtice representa a cada uno de los personajes y cada arista representa
+ la conexi√≥n entre 2 amigos, es decir, cada par de amigos.
 
  Luego tenemos un vector donde se guarda el valor i-esimo de oro que pide cada personaje para difundir el rumor.
- En la funciÛn dfs, recorremos el grafo, teniendo un vector de booleanos para saber si hemos recorrido un vÈrtice
- o no. Para recorrerlo, visitamos los adyacentes de los adyacentes, llamando a la funciÛn de manera recursiva,
- guardando el valor m·s bajo de todos esos adyacentes.
+ En la funci√≥n dfs, recorremos el grafo, teniendo un vector de booleanos para saber si hemos recorrido un v√©rtice
+ o no. Para recorrerlo, visitamos los adyacentes de los adyacentes, llamando a la funci√≥n de manera recursiva,
+ guardando el valor m√°s bajo de todos esos adyacentes.
 
  @ </answer> */
 
 
  // ================================================================
- // Escribe el cÛdigo completo de tu soluciÛn aquÌ debajo (despuÈs de la marca)
+ // Escribe el c√≥digo completo de tu soluci√≥n aqu√≠ debajo (despu√©s de la marca)
  //@ <answer>
 
 class completarMision {
 public:
     completarMision(Grafo const& g, vector<int> const& vVal) : visit(g.V(), false) {
-        for (int i = 0; i < g.V(); i++) {                                   // Recorremos el n˙mero de vÈrtices del grafo.
+        for (int i = 0; i < g.V(); i++) {                                   // Recorremos el n√∫mero de v√©rtices del grafo.
             if (!visit[i]) {
                 res += dfs(g, i, vVal);
                 minimo = INT_MAX;
@@ -89,7 +89,7 @@ private:
 bool resuelveCaso() {
 
     // leemos la entrada
-    int N, M;           // N es el n˙mero de personajes en Overcity y M es el n˙mero de pares de amigos
+    int N, M;           // N es el n√∫mero de personajes en Overcity y M es el n√∫mero de pares de amigos
     cin >> N >> M;
 
     if (!cin)
@@ -122,7 +122,7 @@ bool resuelveCaso() {
 }
 
 //@ </answer>
-//  Lo que se escriba dejado de esta lÌnea ya no forma parte de la soluciÛn.
+//  Lo que se escriba dejado de esta l√≠nea ya no forma parte de la soluci√≥n.
 
 int main() {
     // ajustes para que cin extraiga directamente de un fichero
