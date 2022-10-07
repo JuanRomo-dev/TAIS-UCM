3abd54b2efcfb56e3a4762be107f8b6fa743e49a
LordSama <72462333+LordSama@users.noreply.github.com>
2022-10-07 13:17:25 +0200

    Rename Ejercicios Juez/Source.cpp to Ejercicios Juez/08 Pájaros en vuelo/Source.cpp

--- Ejercicios Juez/08 Pájaros en vuelo/Source.cpp
+++ Ejercicios Juez/08 Pájaros en vuelo/Source.cpp
@@ -13,43 +13,43 @@ using namespace std;
 
 /*@ <answer>
 
- Escribe aquÌ un comentario general sobre la soluciÛn, explicando cÛmo
- se resuelve el problema y cu·l es el coste de la soluciÛn, en funciÛn
- del tamaÒo del problema.
+ Escribe aqu√≠ un comentario general sobre la soluci√≥n, explicando c√≥mo
+ se resuelve el problema y cu√°l es el coste de la soluci√≥n, en funci√≥n
+ del tama√±o del problema.
 
- Para resolver el problema, utilizamos dos colas de prioridad. Una para los p·jaros que est·n a la izquierda del que va primero, y la otra estar·
- ordenada de menor a mayor para que en el primer elemento siempre estÈ el primer p·jaro. A continuaciÛn llamaremos a la funciÛn edadPrimero, en la cu·l
- se aÒadir· a la pareja a la bandada seg˙n las edades de los p·jaros, pudiendo ser menor o mayor que el que iba primero.
+ Para resolver el problema, utilizamos dos colas de prioridad. Una para los p√°jaros que est√°n a la izquierda del que va primero, y la otra estar√°
+ ordenada de menor a mayor para que en el primer elemento siempre est√© el primer p√°jaro. A continuaci√≥n llamaremos a la funci√≥n edadPrimero, en la cu√°l
+ se a√±adir√° a la pareja a la bandada seg√∫n las edades de los p√°jaros, pudiendo ser menor o mayor que el que iba primero.
 
- El coste del algoritmo es O((log N) * M) siendo N el n˙mero de p·jaros que hay en total, y M la suma de los elementos en las colas izq y der.
+ El coste del algoritmo es O((log N) * M) siendo N el n√∫mero de p√°jaros que hay en total, y M la suma de los elementos en las colas izq y der.
 
  @ </answer> */
 
 
  // ================================================================
- // Escribe el cÛdigo completo de tu soluciÛn aquÌ debajo
+ // Escribe el c√≥digo completo de tu soluci√≥n aqu√≠ debajo
  // ================================================================
  //@ <answer>
 
 void edadPrimero(PriorityQueue<int, greater<int>>& pajarosLadoIzq, PriorityQueue<int>& pajarosLadoDer, int aux1, int aux2) {
-    int primer = pajarosLadoDer.top();      // Cogemos el p·jaro que va primero.
+    int primer = pajarosLadoDer.top();      // Cogemos el p√°jaro que va primero.
     int aux;
 
     // Ahora dividimos en casos para ver a donde colocamos a cada uno de los miembros de la pareja, si a la derecha o a la izquierda.
     if (aux1 < primer && aux2 < primer) {
-        pajarosLadoIzq.push(aux1);          // Como los 2 p·jaros de la pareja son mas jÛvenes que el primero, se colocan a la izquierda.
+        pajarosLadoIzq.push(aux1);          // Como los 2 p√°jaros de la pareja son mas j√≥venes que el primero, se colocan a la izquierda.
         pajarosLadoIzq.push(aux2);
 
-        // Como ahora hay m·s a la izquierda que a la derecha, reordenamos, colocando a uno de la izquierda a la derecha.
+        // Como ahora hay m√°s a la izquierda que a la derecha, reordenamos, colocando a uno de la izquierda a la derecha.
         aux = pajarosLadoIzq.top();
         pajarosLadoIzq.pop();
         pajarosLadoDer.push(aux);
     }
-    else if (aux1 > primer && aux2 > primer) {  // Los 2 p·jaros tienen m·s edad que el que est· al frente, asÌ que se coloca la pareja a la derecha.
+    else if (aux1 > primer && aux2 > primer) {  // Los 2 p√°jaros tienen m√°s edad que el que est√° al frente, as√≠ que se coloca la pareja a la derecha.
         pajarosLadoDer.push(aux1);
         pajarosLadoDer.push(aux2);
 
-        // Como ahora hay m·s p·jaros a la derecha que a la izquierda, reordenamos, coloando a uno de la derecha a la izquierda.
+        // Como ahora hay m√°s p√°jaros a la derecha que a la izquierda, reordenamos, coloando a uno de la derecha a la izquierda.
         aux = pajarosLadoDer.top();
         pajarosLadoDer.pop();
         pajarosLadoIzq.push(aux);
@@ -67,32 +67,32 @@ void edadPrimero(PriorityQueue<int, greater<int>>& pajarosLadoIzq, PriorityQueue
 bool resuelveCaso() {
 
     // leer los datos de la entrada
-    int edadPrimer, nParejas;           // edadPrimer es la edad del primer p·jaro que echa a volar, y nParejas es el n˙mero de parejas que se ir·n uniendo despuÈs.
+    int edadPrimer, nParejas;           // edadPrimer es la edad del primer p√°jaro que echa a volar, y nParejas es el n√∫mero de parejas que se ir√°n uniendo despu√©s.
 
     cin >> edadPrimer >> nParejas;
     if (edadPrimer == 0 && nParejas == 0)   // Caso final.
         return false;
 
     int aux1, aux2;                         // Para leer cada uno de los miembros de la pareja.
-    PriorityQueue<int, greater<int>>  pajarosLadoIzq;     // Cola de prioridad para los p·jaros a la izquierda del primero.
-    PriorityQueue<int> pajarosLadoDer;     // Cola de prioridad para los p·jaros a la derecha del primero.
-    pajarosLadoDer.push(edadPrimer);        // Guardamos al p·jaro que est· primero en la cola de la derecha, ya que como est· ordenada de menor a mayor, el primer elemento siempre ser· el primer p·jaro.
+    PriorityQueue<int, greater<int>>  pajarosLadoIzq;     // Cola de prioridad para los p√°jaros a la izquierda del primero.
+    PriorityQueue<int> pajarosLadoDer;     // Cola de prioridad para los p√°jaros a la derecha del primero.
+    pajarosLadoDer.push(edadPrimer);        // Guardamos al p√°jaro que est√° primero en la cola de la derecha, ya que como est√° ordenada de menor a mayor, el primer elemento siempre ser√° el primer p√°jaro.
     for (int i = 0; i < nParejas; i++) {
         cin >> aux1 >> aux2;                // Leemos los miembros de la pareja.
-        edadPrimero(pajarosLadoIzq, pajarosLadoDer, aux1, aux2);        // FunciÛn para ver como colocar a los p·jaros.
+        edadPrimero(pajarosLadoIzq, pajarosLadoDer, aux1, aux2);        // Funci√≥n para ver como colocar a los p√°jaros.
         cout << pajarosLadoDer.top() << " ";
     }
     cout << '\n';
 
     // resolver el caso posiblemente llamando a otras funciones
 
-    // escribir la soluciÛn
+    // escribir la soluci√≥n
 
     return true;
 }
 
 //@ </answer>
-//  Lo que se escriba dejado de esta lÌnea ya no forma parte de la soluciÛn.
+//  Lo que se escriba dejado de esta l√≠nea ya no forma parte de la soluci√≥n.
 
 int main() {
     // ajustes para que cin extraiga directamente de un fichero
