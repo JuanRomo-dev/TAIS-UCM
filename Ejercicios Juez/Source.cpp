f75ffa41fd52683484afad7e296c04f51a30cdee
LordSama <72462333+LordSama@users.noreply.github.com>
2022-10-11 14:12:52 +0300

    Add files via upload

---
+++ Ejercicios Juez/Source.cpp
@@ -0,0 +1,65 @@
+
+/*@ <answer>
+ *
+ * Nombre y Apellidos: Juan Romo Iribarren
+ *
+ *@ </answer> */
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+bool resuelveCaso() {
+
+    // leer los datos de la entrada
+    int V, A;              // V es el número de vértices del grafo y A el número de aristas.
+
+    cin >> V >> A;
+
+    if (!std::cin)  // fin de la entrada
+        return false;
+
+    for (int i = 0; i < A; i++) {
+
+    }
+
+    // resolver el caso posiblemente llamando a otras funciones
+
+    // escribir la solución
+
+    return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+    // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+    std::ifstream in("casos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+    while (resuelveCaso());
+
+    // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+    return 0;
+}
