42e5cf4ddf3c47087f3d4b9729aa02ead082a61e
Juan <72462333+LordSama@users.noreply.github.com>
2023-01-08 17:10:28 +0100

    Add files via upload

--- Ejercicios Juez/26 Navegando sin teclado/Source.cpp
+++ Ejercicios Juez/26 Navegando sin teclado/Source.cpp
@@ -13,27 +13,37 @@ using namespace std;
 
 /*@ <answer>
 
- Escribe aquÃ­ un comentario general sobre la soluciÃ³n, explicando cÃ³mo
- se resuelve el problema y cuÃ¡l es el coste de la soluciÃ³n, en funciÃ³n
- del tamaÃ±o del problema.
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ Este problema consiste en que tenemos una serie de webs a las que queremosa acceder, de tal manera que están conectadas mediante enlaces entre sí, con un tiempo de carga determinado. Queremos
+ averiguar si se puede llegar a la web deseada y el tiempo mínimo que llevaría a ella. 
+
+ Para resolverlo, utilizamos un grafo valorado dirigido, en el que los vértices son las webs, las aristas señalan de que página a otra se puede ir, y el valor es el tiempo que se tarda en
+ encontrar el enlace en la web desde que ha cargado la página. Los tiempos de carga de cada página están almacenados en el vector tiemposCarga. Utilizamos el algoritmo de Dijkstra para
+ conocer el camino de coste mínimo desde un vértice origen hasta el vértice buscado.
+
+ El coste del algoritmo es O(A * log(V)), donde A son las aristas y V los vértices
+
 
  @ </answer> */
 
 
  // ================================================================
- // Escribe el cÃ³digo completo de tu soluciÃ³n aquÃ­ debajo
+ // Escribe el código completo de tu solución aquí debajo
  // ================================================================
  //@ <answer>
 
 class Navegar {
 public:
     Navegar(DigrafoValorado<int> const& grafo, vector<int> const& tiemposCarga) : dist(grafo.V(), INF), pq(grafo.V()) {
-        dist[0] = tiemposCarga[0];
-        pq.push(0, 0);
+        dist[0] = tiemposCarga[0];      // Marcamos la distancia de la primera página con su tiempo de carga.
+        pq.push(0, 0);          // La añadimos a la pq.
         while (!pq.empty()) {
             int v = pq.top().elem;
             pq.pop();
-            for (auto a : grafo.ady(v)) {
+            for (auto a : grafo.ady(v)) {       // Exploramos los adyacentes para relajar las aristas.
                 relajar(a, tiemposCarga);
             }
         }
@@ -48,13 +58,13 @@ public:
     }
 
 private:
-    const int INF = std::numeric_limits<int>::max();
-    vector<int> dist;
+    const int INF = std::numeric_limits<int>::max();        // Las distancias al principio valen infinito.
+    vector<int> dist;       // Para marcar la distancia de cada vértice respecto al origen.
     IndexPQ<int> pq;
 
     void relajar(AristaDirigida<int> a, vector<int> tiemposCarga) {
         int v = a.desde(), w = a.hasta();
-        if (dist[w] > dist[v] + a.valor() + tiemposCarga[w]) {
+        if (dist[w] > dist[v] + a.valor() + tiemposCarga[w]) {      // Comprobamos si la distancia es mejor que las otras encontradas
             dist[w] = dist[v] + a.valor() + tiemposCarga[w];
             pq.update(w, dist[w]);
         }
@@ -63,7 +73,7 @@ private:
 
 bool resuelveCaso() {
     // leer los datos de la entrada
-    int N;      // NÃºmero de pÃ¡ginas manejadas.
+    int N;      // Número de páginas manejadas.
 
     cin >> N;
     if (N == 0)
@@ -74,11 +84,11 @@ bool resuelveCaso() {
         cin >> tiemposCarga[i];
     }
 
-    int M;      // NÃºmero de enlaces entre pÃ¡ginas que se han identificado.
+    int M;      // Número de enlaces entre páginas que se han identificado.
     cin >> M;
 
     DigrafoValorado<int> grafo(N);
-    int u, v, tiempo;       // PÃ¡gina en la que se encuentra, pÃ¡gina a la que se va y tiempo que pasa desde que carga la pÃ¡gina hasta que se encuentra el enlace.
+    int u, v, tiempo;       // Página en la que se encuentra, página a la que se va y tiempo que pasa desde que carga la página hasta que se encuentra el enlace.
 
     for (int i = 0; i < M; i++) {
         cin >> u >> v >> tiempo;
@@ -88,7 +98,7 @@ bool resuelveCaso() {
     Navegar nav(grafo, tiemposCarga);
 
     // resolver el caso posiblemente llamando a otras funciones
-    // escribir la soluciÃ³n
+    // escribir la solución
     if (nav.hayCamino()) {
         cout << nav.tiempoNavegacion();
     }
@@ -101,7 +111,7 @@ bool resuelveCaso() {
 }
 
 //@ </answer>
-//  Lo que se escriba dejado de esta lÃ­nea ya no forma parte de la soluciÃ³n.
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
 
 int main() {
     // ajustes para que cin extraiga directamente de un fichero
