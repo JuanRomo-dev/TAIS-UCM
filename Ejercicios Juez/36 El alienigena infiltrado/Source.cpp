41627b42dcd5e0bb803da7be66538680c09fab35
Juan <72462333+LordSama@users.noreply.github.com>
2023-01-11 13:01:59 +0100

    Rename Ejercicios Juez/Source.cpp to Ejercicios Juez/36 El alienigena infiltrado/Source.cpp

--- Ejercicios Juez/36 El alienigena infiltrado/Source.cpp
+++ Ejercicios Juez/36 El alienigena infiltrado/Source.cpp
@@ -12,15 +12,15 @@ using namespace std;
 
 /*@ <answer>
 
- Escribe aquÌ un comentario general sobre la soluciÛn, explicando cÛmo
- se resuelve el problema y cu·l es el coste de la soluciÛn, en funciÛn
- del tamaÒo del problema.
+ Escribe aqu√≠ un comentario general sobre la soluci√≥n, explicando c√≥mo
+ se resuelve el problema y cu√°l es el coste de la soluci√≥n, en funci√≥n
+ del tama√±o del problema.
 
  @ </answer> */
 
 
  // ================================================================
- // Escribe el cÛdigo completo de tu soluciÛn aquÌ debajo
+ // Escribe el c√≥digo completo de tu soluci√≥n aqu√≠ debajo
  // ================================================================
  //@ <answer>
 
@@ -51,13 +51,13 @@ int resolver(vector<pair<int,int>> const& trabajo, int C, int F, bool& b) {
 
 bool resuelveCaso() {
     // leer los datos de la entrada
-    int C, F, N;        // Comienzo y final del intervalo en el que el planeta est· expuesto, n˙mero de trabajos de investigaciÛn propuestos.
+    int C, F, N;        // Comienzo y final del intervalo en el que el planeta est√° expuesto, n√∫mero de trabajos de investigaci√≥n propuestos.
 
     cin >> C >> F >> N;
     if (C == 0 && F == 0 && N == 0)
         return false;
 
-    int ci, fi;     // Comienzo y final de los intervalos en que cada trabajo utilizarÌa el telescopio.
+    int ci, fi;     // Comienzo y final de los intervalos en que cada trabajo utilizar√≠a el telescopio.
     vector<pair<int, int>> trabajos(N); // Vector de pares que contiene en el primer valor el comienzo del intervalo de un trabajo y el segundo valor, el final.
     for (int i = 0; i < N; i++) {
         cin >> ci >> fi;
@@ -70,7 +70,7 @@ bool resuelveCaso() {
     // resolver el caso posiblemente llamando a otras funciones
     int sol = resolver(trabajos, C, F, b);
 
-    // escribir la soluciÛn
+    // escribir la soluci√≥n
     if (!b) {
         cout << sol;
     }
@@ -83,7 +83,7 @@ bool resuelveCaso() {
 }
 
 //@ </answer>
-//  Lo que se escriba dejado de esta lÌnea ya no forma parte de la soluciÛn.
+//  Lo que se escriba dejado de esta l√≠nea ya no forma parte de la soluci√≥n.
 
 int main() {
     // ajustes para que cin extraiga directamente de un fichero
